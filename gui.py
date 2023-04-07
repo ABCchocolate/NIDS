49542b4eb7fc3e49065142b22422d3095d5467d5
ABCchocolate <88300899+ABCchocolate@users.noreply.github.com>
2023-04-07 15:01:12 +0900

    dtd

--- gui.py
+++ gui.py
@@ -1,30 +1,16 @@
-import tkinter as tk
-import tkinter.filedialog as filedialog
-from tensorflow.keras.models import load_model
-import numpy as np
+from tkinter import *
 
-# 모델 로드
-model = load_model('../NIDS-1/weights/mlp_multi.h5')
+# Tk 객체 생성
+root = Tk()
 
-# GUI 애플리케이션 생성
-root = tk.Tk()
+# 리스트 박스 추가
+listbox = Listbox(root)
+listbox.pack()
 
-# 파일 선택 대화상자 열기
-def open_file():
-    file_path = filedialog.askopenfilename()
-    if file_path:
-        # 이미지 불러오기
-        image = tk.PhotoImage(file=file_path)
+# 리스트 박스에 아이템 추가
+listbox.insert(END, "Item 1")
+listbox.insert(END, "Item 2")
+listbox.insert(END, "Item 3")
 
-        # 이미지 예측
-        image_data = np.array(image)
-        prediction = model.predict(image_data)
-
-        # 예측 결과 출력
-        print(prediction)
-
-# 파일 선택 버튼 생성
-button = tk.Button(root, text='Open File', command=open_file)
-button.pack()
-
+# Tk 객체 실행
 root.mainloop()
